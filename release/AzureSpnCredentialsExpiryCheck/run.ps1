8c7fa4842661b2c9dae622fe5cbdecb271a40c2f
matoy <matoy@users.noreply.github.com>
2021-08-26 18:43:07 +0200

    Update run.ps1

--- release/AzureSpnCredentialsExpiryCheck/run.ps1
+++ release/AzureSpnCredentialsExpiryCheck/run.ps1
@@ -53,6 +53,27 @@ function CheckCred {
 	return $null
 }
 
+function Send-EmailWithSendGrid {
+    Param (
+        [Parameter(Mandatory=$true)] [string] $From,
+        [Parameter(Mandatory=$true)] [String] $To,
+        [Parameter(Mandatory=$true)] [string] $ApiKey,
+        [Parameter(Mandatory=$true)] [string] $Subject,
+        [Parameter(Mandatory=$true)] [string] $Body
+    )
+    $headers = @{}
+    $headers.Add("Authorization","Bearer $apiKey")
+    $headers.Add("Content-Type", "application/json")
+    $jsonRequest = [ordered]@{
+		personalizations= @(@{to = @(@{email =  "$To"})
+			subject = "$SubJect" })
+			from = @{email = "$From"}
+			content = @( @{ type = "text/plain"
+						value = "$Body" }
+			)} | ConvertTo-Json -Depth 10
+    Invoke-RestMethod   -Uri "https://api.sendgrid.com/v3/mail/send" -Method Post -Headers $headers -Body $jsonRequest 
+}
+
 $warning = [int] $Request.Query.Warning
 if (-not $warning) {
     $warning = 30
